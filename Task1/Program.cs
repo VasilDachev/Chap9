cd8a8cf13d4bc6e8295a296842dbce700c9d2f60
VasilDachev <vasildachevnew@gmail.com>
2018-11-27 12:00:17 +0200

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void ReturnName(string name)
+        {
+            Console.WriteLine("Hello {0}", name);
+        }
+
+
+        static void Main(string[] args)
+        {
+            /*Напишете метод, който при подадено име отпечатва на конзолата
+            "Hello, <name>!" (например "Hello, Peter!"). Напишете програма,
+            която тества дали този метод работи правилно.*/
+            Console.Write("Enter name: ");
+            ReturnName(Console.ReadLine());
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+
+
+        /*Създайте метод GetMax() с два целочислени (int) параметъра, който връща по-голямото от двете числа. Напишете програма, която прочита три цели числа от конзолата и отпечатва най-голямото от тях, изпол­звайки метода GetMax().
+         */
+        static int a;
+
+        static void GetMax(int first, int second)
+        {
+            if (first > second) a = first;
+            else a = second;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            int c = Int32.Parse(Console.ReadLine());
+
+            GetMax(a, b);
+            GetMax(a, c);
+
+            Console.WriteLine("Biggest number is {0}", a);
+        }
+
+
+    }
+}
